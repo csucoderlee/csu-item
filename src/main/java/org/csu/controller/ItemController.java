d57ad715f2be1549a0bba9fed8c55f404ec333d5
lixiang <csucoderlee@gmail.com>
2017-07-09 23:43:16 +0800

    供另外一个服务调用

--- src/main/java/org/csu/controller/ItemController.java
+++ src/main/java/org/csu/controller/ItemController.java
@@ -9,8 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ItemController {
 
-    @GetMapping
-    public Object itemDetail() {
-        return "hello csu-item itemdetail";
+    @GetMapping("getProductInfo")
+    public Object itemDetail(Long productId) {
+        return "{\"id\": " + productId + ", " +
+                "\"name\": \"iphone7手机\", \"price\": 5599, " +
+                "\"pictureList\":\"a.jpg,b.jpg\"," +
+                " \"specification\": \"iphone7的规格\"," +
+                " \"service\": \"iphone7的售后服务\", " +
+                "\"color\": \"红色,白色,黑色\", " +
+                "\"size\": \"5.5\", " +
+                "\"shopId\": 1, " +
+                "\"modifiedTime\": \"2017-01-01 12:00:00\"}";
     }
 }
